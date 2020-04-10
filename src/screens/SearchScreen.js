0e589bee0d4cd7d1dbe1eeaf244cb0b17fba9115
Luke Russell <thelukerussel21@gmail.com>
2020-04-10 15:10:52 -0400

    deleted commented code

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -7,28 +7,6 @@ import useResults from '../hooks/useResults';
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [searchApi, results, errorMessage] = useResults();
-  // const [results, setResults] = useState([]);
-  // const [errorMessage, setErrorMessage] = useState('');
-
-  // const searchApi = async (searchTerm) => {
-  //   try {
-  //     const response = await yelp.get('/search', {
-  //       params: {
-  //         limit: 50,
-  //         term: searchTerm,
-  //         location: 'los angeles',
-  //       },
-  //     });
-  //     setResults(response.data.businesses);
-  //   } catch (err) {
-  //     // console.log(err);
-  //     setErrorMessage('Something went wrong');
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   searchApi('pasta');
-  // }, []);
 
   return (
     <View>
