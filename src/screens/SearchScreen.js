ffa62d0ae7647fbb11dbb7a2220a64a0ec8759a0
Luke Russell <thelukerussel21@gmail.com>
2020-04-10 14:44:27 -0400

    axios search request added

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,17 +1,27 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get('/search', {
+      params: {
+        limit: 50,
+        term: term,
+        location: 'los angeles',
+      },
+    });
+    setResults(response.data.businesses);
+  };
+
   return (
     <View>
-      <SearchBar
-        onTermSubmit={() => console.log('Term was Submitted')}
-        term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-      />
-      <Text>{term}</Text>
+      <SearchBar onTermSubmit={searchApi} term={term} onTermChange={setTerm} />
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
