79a992f4669aa3c0147a1afa90534c4597be1b93
Luke Russell <thelukerussel21@gmail.com>
2020-04-10 15:10:33 -0400

    added hooks folder

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,32 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [searchApi, results, errorMessage] = useResults();
+  // const [results, setResults] = useState([]);
+  // const [errorMessage, setErrorMessage] = useState('');
 
-  const searchApi = async () => {
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term: term,
-          location: 'los angeles',
-        },
-      });
-      setResults(response.data.businesses);
-    } catch (err) {
-      // console.log(err);
-      setErrorMessage('Something went wrong');
-    }
-  };
+  // const searchApi = async (searchTerm) => {
+  //   try {
+  //     const response = await yelp.get('/search', {
+  //       params: {
+  //         limit: 50,
+  //         term: searchTerm,
+  //         location: 'los angeles',
+  //       },
+  //     });
+  //     setResults(response.data.businesses);
+  //   } catch (err) {
+  //     // console.log(err);
+  //     setErrorMessage('Something went wrong');
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   searchApi('pasta');
+  // }, []);
 
   return (
     <View>
-      <SearchBar onTermSubmit={searchApi} term={term} onTermChange={setTerm} />
+      <SearchBar
+        onTermSubmit={() => searchApi(term)}
+        term={term}
+        onTermChange={setTerm}
+      />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
